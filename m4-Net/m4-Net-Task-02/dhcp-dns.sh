d2694f9e94ed64d92147fd4437d5a8dfe1b7aee6
Yurii Pelykh <yurii_pelykh@epam.com>
2022-07-11 22:57:14 +0300

    m4-Net-Task-02, DHCP-Server 1 scope provision done

---
+++ m4-Net/m4-Net-Task-02/dhcp-dns.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+INT1="eth1"
+
+
+apt_install () {
+    apt-get update
+    apt-get install isc-dhcp-server -y
+}
+
+interfaces_config() {
+    touch /etc/netplan/02-netcfg.yaml
+    echo "network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    ${INT1}:
+      addresses: [172.16.24.253/24]
+      gateway4: 172.16.24.254
+      nameservers:
+        addresses: [127.0.0.1]
+        search:
+        - rocca.local" > /etc/netplan/02-netcfg.yaml
+
+    ip link set ${INT1} up
+    netplan apply
+}
+
+
+change-config-file() {
+cp $5{,.bak}
+while IFS='' read -r a
+    do
+    if [[ $(echo $a | grep -q $2 ; echo $?) != 0 ]]; then
+        echo "${a//$1/$1 $2=$4}"
+    else
+        if [[ $(expr index "$a" $'\t' > /dev/null; echo $?) == 0  ]]; then
+            r=$(echo $a | grep -oP "(# )*$2[ =\t]{1,3}([a-z0-9\"]+)*" | cut -d' ' -f1,2 --output-delimiter=$'\t')
+        else
+            r=$(echo $a | grep -oP "(# )*$2[ =\t]{1,3}([a-z0-9\"]+)*")
+        fi
+        echo -e "${a//$r/$2$3\"$4\"}"
+    fi
+    done < $5 > $5.t
+mv $5{.t,}
+}
+
+
+dhcp_server_config() {
+    echo "subnet 172.16.24.0 netmask 255.255.255.0 {
+     range 172.16.24.1 172.16.24.100;
+     option subnet-mask 255.255.255.0;
+     option broadcast-address 172.16.24.255;
+     option domain-name-servers 172.16.24.253;
+     option domain-name \"rocca.local\";
+     option routers 172.16.24.254;
+     default-lease-time 7200;
+     max-lease-time 480000;
+
+     host Rgw {
+      hardware ethernet 08:00:27:9b:0d:35;
+      fixed-address 172.16.24.254;
+     }
+}" >> /etc/dhcp/dhcpd.conf
+
+    change-config-file "" "INTERFACESv4" "=" "${INT1}" "/etc/default/isc-dhcp-server"
+
+    systemctl restart isc-dhcp-server
+}
+
+
+routing_config() {
+    ip route del default via 10.0.2.2
+}
+
+apt_install
+interfaces_config
+dhcp_server_config
+routing_config
+
+exit 0
--- m4-Net/m4-Net-Task-02/rgw.sh
+++ m4-Net/m4-Net-Task-02/rgw.sh
@@ -6,17 +6,49 @@ ip4_forwarding_enable() {
     sysctl -p /etc/sysctl.conf
 }
 
+
+change-config-file() {
+cp $5{,.bak}
+while IFS='' read -r a
+    do
+    if [[ $(echo $a | grep -q $2 ; echo $?) != 0 ]]; then
+        echo "${a//$1/$1 $2=$4}"
+    else
+        if [[ $(expr index "$a" $'\t' > /dev/null; echo $?) == 0  ]]; then
+            r=$(echo $a | grep -oP "(# )*$2[ =\t]{1,3}([a-z0-9\"]+)*" | cut -d' ' -f1,2 --output-delimiter=$'\t')
+        else
+            r=$(echo $a | grep -oP "(# )*$2[ =\t]{1,3}([a-z0-9\"]+)*")
+        fi
+        echo -e "${a//$r/$2$3\"$4\"}"
+    fi
+    done < $5 > $5.t
+mv $5{.t,}
+}
+
+
 #Network interfaces configuration:
 interfaces_config() {
     touch /etc/sysconfig/network-scripts/ifcfg-eth1
-    echo 'TYPE="Ethernet"\nBOOTPROTO="dhcp"\nDEFROUTE="yes"\nIPV4_FAILURE_FATAL="no"NAME="eth1"\nDEVICE="eth1"\nONBOOT="yes"' > /etc/sysconfig/network-scripts/ifcfg-eth1
-    #ifdown eth1
-    #ifup eth1
+    echo 'TYPE="Ethernet"
+    BOOTPROTO="dhcp"
+    DEFROUTE="yes"
+    IPV4_FAILURE_FATAL="no"
+    NAME="eth1"
+    DEVICE="eth1"
+    ONBOOT="yes"' > /etc/sysconfig/network-scripts/ifcfg-eth1
 
     touch /etc/sysconfig/network-scripts/ifcfg-eth2
-    echo 'TYPE="Ethernet"\nBOOTPROTO="dhcp"\nDEFROUTE="yes"\nIPV4_FAILURE_FATAL="no"NAME="eth2"\nDEVICE="eth2"\nONBOOT="yes"' > /etc/sysconfig/network-scripts/ifcfg-eth2
-    #ifdown eth2
-    #ifup eth2
+    echo 'TYPE="Ethernet"
+    BOOTPROTO="dhcp"
+    DEFROUTE="no"
+    IPV4_FAILURE_FATAL="no"
+    NAME="eth2"
+    DEVICE="eth2"
+    ONBOOT="yes"' > /etc/sysconfig/network-scripts/ifcfg-eth2
+
+    change-config-file "" "DEFROUTE" "=" "no" "/etc/sysconfig/network-scripts/ifcfg-eth0"
+
+    systemctl restart NetworkManager.service
 }
 
 #Firewall config:
@@ -104,13 +136,14 @@ firewall_config() {
 }
 
 routing_config() {
-    
+    ip route del default via 10.0.2.2
 }
 
 ip4_forwarding_enable
 interfaces_config
-systemctl restart networking
+systemctl restart NetworkManager.service
 firewall_config
+routing_config
 
 exit 0
 
